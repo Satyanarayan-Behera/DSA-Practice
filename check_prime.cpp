5e5de34b9c0751d1252e026b1fab6b961199fe86
Satyanarayan Behera <satyanarayanbehera075@gmail.com>
2026-07-28 00:24:47 +0530

    Refactor prime checking logic and improve input handling

--- check_prime.cpp
+++ check_prime.cpp
@@ -1,23 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
-int prime(int n){
-    for(int i=2; i<=(n-1); i++){
-        if(n%i==0){
-            return 1;
-        }
-        else{
-            return 0;
-        }
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) return false;
     }
+    return true;
 }
 
-int main(){
-    int n = 8;
-    int r = prime(n);
-    if(r==1){
-        printf("%d is not a prime number.\n" , n);
-    }else{
-        printf("%d is a prime number.\n" , n);
-    }
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPrime(num))
+        cout << num << " is a prime number." << endl;
+    else
+        cout << num << " is not a prime number." << endl;
+
     return 0;
 }
