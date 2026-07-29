4da1a8ea2ab5f5c59d398ce1f06e1fca09ae497e
Satyanarayan Behera <Satyanarayanbehera075@gmail.com>
2026-07-29 07:36:09 +0530

    Adding new sorting problems

--- Array_operations.cpp
+++ Array_operations.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 int garr[] = {1 , 3 , 5 , 12 , 75 , 25}; 
 int arrsize = 6;       //Original and global array 😊
@@ -205,7 +206,7 @@ int main(){
 }*/
 
 
-int findunique(int arr[], int size){
+/*int findunique(int arr[], int size){
     cout<<"Unique values are : ";
     for(int i=0 ; i <size; i++){
         bool found=false;
@@ -227,7 +228,86 @@ int main(){
     int size = 9;
     findunique(arr,size);
     return 0;
+}*/
+
+
+/*void selectionsort(int arr[] , int n){
+    for(int i=0; i<n-1; i++){      // Assuming sorted part
+        int smallestidx = i;
+        for(int j=i+1; j<n; j++){     //Assuming unsorted part
+            if(arr[j] < arr[smallestidx]){
+                smallestidx = j;
+            }
+        }
+        swap(arr[i], arr[smallestidx]);
+    }
 }
+int main(){
+    int arr[] = {4,1,5,2,3};
+    int n =5;
+    selectionsort(arr,n);
+    for(int val : arr){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    return 0;
+}*/
+
+
+/*void insertionsort(int arr[], int size){
+    for(int i=1; i<size; i++){
+        int current = arr[i];
+        int previous = i-1;
+        while(previous >= 0 && arr[previous] > current){
+            arr[previous + 1] = arr[previous];
+            previous--;
+        }
+        arr[previous+1] = current;
+    }
+}
+
+int main(){
+    int arr[] = {4,1,5,2,3};
+    int size = 5;
+    insertionsort(arr, size);
+    for(int val : arr){
+        cout<<val<<" ";
+    }
+    return 0;
+}*/
+
+
+//sort array with 0s, 1s and 2s    time complexity 'O(n)'
+//Approach-1 (Double pass)
+int main(){
+    int arr[]={2,0,2,1,1,0,1,2,0,0};
+    int size = 10;
+    int c0=0,c1=0,c2=0;
+    for(int i=0; i<size; i++){
+        if(arr[i] == 0) c0++;
+        else if(arr[i] == 1) c1++;
+        else c2++;
+    }
+    int idx = 0;
+    for(int i=0; i<c0; i++){
+        arr[idx++] = 0;
+    }
+    for(int i=0; i<c1; i++){
+        arr[idx++] = 1;
+    }
+    for(int i=0; i<c2; i++){
+        arr[idx++] = 2;
+    }
+    
+    for(int val : arr){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    return 0;
+} 
+//Approach-2 (Single pass) <-- 'Dutch National Flag Algo' -TC-O(n) , SC-O(n)
+//Redirect to "Pointers.cpp" file.
+
 
 
 
